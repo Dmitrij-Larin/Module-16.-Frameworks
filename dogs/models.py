73391dcd11501d8815fe77d12477661f66372990
Dmitrij Larin <larin4921@gmail.com>
2025-03-25 22:59:52 +0300

    1) Добавлена модель Dog; 2) Добавлена библиотека Pillow; 3) Созданы и применены миграции.

--- dogs/models.py
+++ dogs/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from users.models import NULLABLE
 
+
 class Breed(models.Model):
     name = models.CharField(max_length=100, verbose_name='breed')
     description = models.CharField(max_length=1000, verbose_name='description', **NULLABLE)
@@ -12,3 +13,24 @@ class Breed(models.Model):
     class Meta:
         verbose_name = 'breed'
         verbose_name_plural = 'breeds'
+
+
+class Dog(models.Model):
+    name = models.CharField(max_length=150, verbose_name='dog_name')
+    breed = models.ForeignKey(Breed, on_delete=models.CASCADE, verbose_name='breed')
+    photo = models.ImageField(upload_to='dogs/', **NULLABLE, verbose_name='image')
+    birth_date = models.DateField(**NULLABLE, verbose_name='birth_date')
+
+    def __str__(self):
+        return f'{self.name} ({self.breed})'
+
+    class Meta:
+        verbose_name = 'dog'
+        verbose_name_plural = 'dogs'
+        # abstract = True
+        # app_label = 'dogs'
+        # ordering = [-1]
+        # permission = []
+        # db_table = 'doggies'
+        # get_latest_by = 'birth_date'
+
