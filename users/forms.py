e8e05513cb9166c1666667adcf748bf520168429
Dmitrij Larin <larin4921@gmail.com>
2025-04-02 05:15:56 +0300

    Добавлен функционал регистрации пользователя (полностью)

--- users/forms.py
+++ users/forms.py
@@ -8,3 +8,20 @@ class StyleFromMixin:
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
+
+
+class UserRegisterForm(StyleFromMixin, forms.ModelForm):
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('email',)
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Пароли не совпадают!!!')
+        return cd['password2']
+
+
