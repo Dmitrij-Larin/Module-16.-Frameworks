2f1e7fb5f297c2d46697383077e8c9cb8dce9056
Dmitrij Larin <larin4921@gmail.com>
2025-04-26 01:26:56 +0300

    1) Добавлена форма DogAdminForm. 2) Добавлен вывод разных форм в DogUpdateView в зависимости от уровня доступа.

--- dogs/forms.py
+++ dogs/forms.py
@@ -11,13 +11,23 @@ class DogForm(StyleFromMixin, forms.ModelForm):
         model = Dog
         exclude = ('owner', 'is_active', 'views',)
 
-    def clean_birth_data(self):
+    def clean_birth_date(self):
         cleaned_data = self.cleaned_data['birth_date']
         if cleaned_data:
             now_year = datetime.datetime.now().year
             if now_year - cleaned_data.year > 35:
                 raise forms.ValidationError('Собака должна быть моложе 35 лет')
         return cleaned_data
+
+
+class DogAdminForm(DogForm):
+    class Meta:
+        model = Dog
+        exclude = ('is_active',)
+
+    # def clean_birth_date(self):
+    #     cleaned_birth_date = super().clean_birth_date()
+    #     return cleaned_birth_date
 
 
 class DogParentForm(StyleFromMixin, forms.ModelForm):
