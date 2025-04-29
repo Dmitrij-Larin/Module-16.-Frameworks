451217a201295640d6f23b98354430ceeb043904
Dmitrij Larin <larin4921@gmail.com>
2025-04-29 03:16:52 +0300

    Добавлен CRUD для Reviews

--- reviews/views.py
+++ reviews/views.py
@@ -8,6 +8,7 @@ from redis.commands.search.querystring import querystring
 from reviews.models import Review
 from users.models import User
 from reviews.forms import ReviewForm
+from  users.models import UserRoles
 
 
 class ReviewListView(ListView):
@@ -34,3 +35,47 @@ class ReviewDeactivatedListView(ListView):
         queryset = super().get_queryset()
         queryset = queryset.filter(sign_of_review=False)
         return queryset
+
+
+class ReviewCreateView(LoginRequiredMixin, CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = 'reviews/create.html'
+    extra_context = {
+        'title': "Написать отзыв"
+    }
+
+
+class ReviewDetailView(LoginRequiredMixin, DetailView):
+    model = Review
+    template_name = 'reviews/detail.html'
+    extra_context = {
+        'title': "Просмотр отзыва"
+    }
+
+
+class ReviewUpdateView(LoginRequiredMixin, UpdateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = 'reviews/update.html'
+    extra_context = {
+        'title': "Изменить отзыв"
+    }
+
+    def get_success_url(self):
+        return reverse('review:review_detail')
+    
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset=queryset)
+        if self.object.author != self.request.user and self.request.user not in [UserRoles.ADMIN, UserRoles.MODERATOR]:
+            raise PermissionDenied()
+        return self.object
+
+
+class ReviewDeleteView(PermissionRequiredMixin, DeleteView):
+    model = Review
+    template_name = 'reviews/delete.html'
+    permission_required = 'reviews.delete_review'
+
+    def get_success_url(self):
+        return reverse('reviews:reviews_list')
